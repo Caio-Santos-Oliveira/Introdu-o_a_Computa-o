35ddb77fc183f3ae0c7d362c657538ffda609bbc
Caio <caio.santosoliveira@hotmail.com>
2022-11-08 23:02:40 -0300

    primeirocommit

---
+++ JavaScript/aula.js
@@ -0,0 +1,49 @@
+var a1 = 15;
+alert(a1);
+alert(typeof a1);
+var a2;
+alert(a2);
+alert(typeof a2);
+var a3 = true;
+alert(a3);
+alert(typeof a3);
+var a4 = "teste";
+alert(a4);
+alert(typeof a4);
+alert("3.Quando um valor de uma variável será undefined?");
+alert("Resposta: var number;");
+alert("4.O que é NaN?");
+alert("Resposta:  NaN (acrônimo em inglês para Not a Number) é um valor ou símbolo usado nas linguagens de programação para representar um valor numérico indefinido ou irrepresentável.");
+var n1 = 342;
+var n2 = 222;
+var n3 = 754;
+var media = (n1+n2+n3)/3;
+alert(media);
+var ap1 = 1.5;
+var ap2 = 2.0;
+var as = 5;
+var mediafinal = ap1+ap2+as;
+var nome = "Joãozinho";
+var idade = 13;
+if (idade>=18){
+    exe7 = nome + " é maior de idade";
+}
+else{
+    exe7 = nome + " é menor de idade";
+}
+alert(exe7);
+if (n1>n2) {
+    if (n1>n3){
+        var maior = n1;
+    }else{
+        var maior = n3;
+    }
+}else{
+    if (n2>n3){
+        var maior = n2;
+    }
+    else{
+        var maior = n3;
+    }
+}
+alert(maior);
